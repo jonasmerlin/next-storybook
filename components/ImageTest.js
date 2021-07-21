da600333bc25654abcbb879e14b2024e4b344570
Jonas Schumacher <jonas.schumacher@my-wingfield.com>
2021-07-21 14:25:21 +0200

    add blur placeholder prop, resulting in 2nd error

--- components/ImageTest.js
+++ components/ImageTest.js
@@ -1,8 +1,13 @@
-import Image from "next/image"
-import testImage from "../public/testImage.jpg"
+import Image from "next/image";
+import testImage from "../public/testImage.jpg";
 
 const ImageTest = () => (
-	<Image src={testImage} alt="A stack of colorful cans" layout="fill" />
-)
+  <Image
+    src={testImage}
+    alt="A stack of colorful cans"
+    layout="fill"
+    placeholder="blur"
+  />
+);
 
-export default ImageTest
+export default ImageTest;
